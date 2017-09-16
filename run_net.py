b13c724466fa84e5e9f5b2bbdb8d27b1e5359825
Arron Cao <qqcao.cs@gmail.com>
2017-09-16 11:31:26 +0100

    add: run net

---
+++ run_net.py
@@ -0,0 +1,140 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import argparse
+from mvnc import mvncapi as mvnc
+import os
+import time
+
+
+def check_file_existence(graph_file_):
+    if not os.path.exists(graph_file_):
+        raise argparse.ArgumentTypeError("%s does not exist!" % graph_file_)
+    print("using graph file:", graph_file_)
+    return graph_file_
+
+
+def check_input_shape(input_shape_):
+    dim_strings = input_shape_.split(",")
+    dims = []
+    for dim_str in dim_strings:
+        if not dim_str:
+            continue
+        try:
+            dim = int(dim_str.strip())
+            dims.append(dim)
+        except Exception as e:
+            print("format error:", e)
+    print("input shape:", input_shape_)
+    return dims
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-g", "--graph_file", type=check_file_existence, default='../networks/AlexNet/graph',
+                        help="model graph file")
+    parser.add_argument("-s", "--input_shape", type=check_input_shape, default='227,227,3',
+                        help="input shape, e.g. 224,224,3")
+    parser.add_argument("-i", "--iterations", type=int, default=1,
+                        help="iterations to run the model")
+    return parser.parse_args()
+
+
+def set_marker(state, channel=10):
+    try:
+        import RPi.GPIO as GPIO
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BOARD)
+        if state == 'clean':
+            print('cleaning up pin states')
+            GPIO.cleanup()
+        else:
+            print('setting up pin:', channel, 'to', state)
+            GPIO.setup(channel, GPIO.OUT, initial=state)
+    except Exception as e:
+        print(e)
+
+
+def get_input_data(input_shape_):
+    import numpy as np
+    return np.random.rand(*input_shape_).astype(np.float16)
+
+
+def get_ncs_device():
+    mvnc.SetGlobalOption(mvnc.GlobalOption.LOGLEVEL, 1)
+    devices = mvnc.EnumerateDevices()
+    if len(devices) == 0:
+        print('No devices found')
+        quit()
+    return mvnc.Device(devices[0])
+
+
+def load_graph(device_, graph_file_):
+    start_time = time.time()
+
+    device_.OpenDevice()
+    opt = device_.GetDeviceOption(mvnc.DeviceOption.OPTIMISATIONLIST)
+    # print('device opt:', opt)
+    with open(graph_file_, mode='rb') as f:
+        blob = f.read()
+    graph_ = device_.AllocateGraph(blob)
+
+    end_time = time.time()
+    return graph_, round((end_time - start_time) * 1000, 2)
+
+
+def load_input(iterations_=1):
+    start_time = time.time()
+
+    graph.SetGraphOption(mvnc.GraphOption.ITERATIONS, iterations_)
+    # print('iter', graph.GetGraphOption(mvnc.GraphOption.ITERATIONS))
+    graph.LoadTensor(input_data, 'user_object')
+
+    end_time = time.time()
+    return round((end_time - start_time) * 1000, 2)
+
+
+def run_inference(graph_):
+    start_time = time.time()
+
+    output_, user_obj = graph_.GetResult()
+
+    end_time = time.time()
+    return output_, round((end_time - start_time) * 1000, 2)
+
+
+def clean_up(device_, graph_):
+    start_time = time.time()
+
+    graph_.DeallocateGraph()
+    device_.CloseDevice()
+
+    end_time = time.time()
+    return round((end_time - start_time) * 1000, 2)
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    graph_file = args.graph_file
+    input_shape = args.input_shape
+    iterations = args.iterations
+
+    set_marker('clean')
+    input_data = get_input_data(input_shape)
+    set_marker(1)
+    device = get_ncs_device()
+    set_marker(0)
+    graph, graph_load_time = load_graph(device, graph_file)
+    print('graph_load_time:', graph_load_time, 'ms')
+    set_marker(1)
+    data_load_time = load_input(iterations)
+    print('data_load_time:', data_load_time, 'ms')
+    set_marker(0)
+    _, inference_time = run_inference(graph)
+    print('inference_time:', inference_time, 'ms')
+    set_marker(1)
+    clean_up_time = clean_up(device, graph)
+    set_marker(0)
+    print('clean_up_time:', clean_up_time, 'ms')
+    set_marker('clean')
+
