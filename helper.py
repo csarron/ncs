057c67dc25a81a037be8d8fb8b95d2745f6331c0
Arron Cao <qqcao.cs@gmail.com>
2017-09-17 21:58:34 +0100

    add: dict merge

--- helper.py
+++ helper.py
@@ -17,3 +17,14 @@ def save_text(weights_file):
     proto_text_file = '{}.txt'.format(weights_file)
     with open(proto_text_file, "w") as f:
         f.write(str(net_def))
+
+
+def merge_dicts(*dict_args):
+    """
+    Given any number of dicts, shallow copy and merge into a new dict,
+    precedence goes to key value pairs in latter dicts.
+    """
+    result = {}
+    for dictionary in dict_args:
+        result.update(dictionary)
+    return result
